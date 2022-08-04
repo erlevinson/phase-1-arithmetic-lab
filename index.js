02f0016cb60d64b007b4e2656eb35b105cecb8a1
Eric <ericlevinson3@gmail.com>
2022-08-04 18:16:49 -0500

    commit tree 907515b53b3d893ee104440a784ac77c2c4eb472

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+const random = 5
+Math.random()
+const num3 = 1
+const num4 = 5
+const mod = num4-num3
+const max = 20
